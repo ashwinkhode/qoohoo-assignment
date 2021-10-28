4a52ea843db601344742b8ee38a99b534784ef96
Ashwin Khode <ashwin.khode1@gmail.com>
2021-10-28 18:33:23 +0530

    add: button to trigger animation

---
+++ src/context/ButtonAnimation.tsx
@@ -0,0 +1,39 @@
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+interface IButtonProviderValue {
+  isVisible: boolean;
+  setVisibility: Dispatch<SetStateAction<boolean>>;
+}
+
+const INITIAL_VALUE: IButtonProviderValue = {
+  isVisible: true,
+  setVisibility: () => {},
+};
+
+const ButtonAnimationContext = createContext(INITIAL_VALUE);
+
+function ButtonProvider({ children }: { children: ReactNode }) {
+  const [isVisible, setVisibility] = useState(true);
+  return (
+    <ButtonAnimationContext.Provider value={{ isVisible, setVisibility }}>
+      {children}
+    </ButtonAnimationContext.Provider>
+  );
+}
+
+function useButtonAnimation() {
+  const context = useContext(ButtonAnimationContext);
+  if (context === undefined) {
+    console.error("useCount must be used within a CountProvider");
+  }
+  return context;
+}
+
+export { ButtonProvider, useButtonAnimation };

edf3410728f116fa8b9adda3961c5860b85f6aa9
Ashwin Khode <ashwin.khode1@gmail.com>
2021-10-28 18:34:10 +0530

    add: context provider to the _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import { useRouter } from "next/router";
+import { AnimatePresence } from "framer-motion";
+import { ButtonProvider } from "../context/ButtonAnimation";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <>
       <Head>
         <title>Qoohoo Assignment</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <AnimatePresence>
+        <ButtonProvider>
+          <Component key={router.pathname} {...pageProps} />
+        </ButtonProvider>
+      </AnimatePresence>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
